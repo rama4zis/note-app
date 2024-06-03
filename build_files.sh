86173db682432fd47bb69a1a4059afff7b987c14
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 08:26:42 +0000

    nroemal

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,3 @@
-# create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
-
-# activate the virtual environment
-source venv/bin/activate
-
 # upgrade pip
 pip install --upgrade pip
 
