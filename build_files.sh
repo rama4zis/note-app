b43681b30dfafdfe56009ac828f16208c1bc09af
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 08:24:29 +0000

    Upgrade pip ver

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,10 @@ python3.9 -m venv venv
 # activate the virtual environment
 source venv/bin/activate
 
+# upgrade pip
+pip install --upgrade pip
+
+# install all the required packages
 pip install -r requirements.txt 
 
 # make migrations
