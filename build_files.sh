34afb198c5d678c818ebbcd92cec8095ed80656a
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 08:10:01 +0000

    update for version

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,6 @@
 # build_files.sh
-pip3 install -r requirements.txt
-pip3 install django --upgrade 
+pip3 install -r requirements.txt 
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+python3 manage.py migrate 
+python3 manage.py collectstatic
