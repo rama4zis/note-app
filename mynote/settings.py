2ab6f50cc9d040457086c9ecc4fb697dc0b01780
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 10:49:37 +0000

    ss

--- mynote/settings.py
+++ mynote/settings.py
@@ -129,11 +129,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = BASE_DIR / 'assets'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = BASE_DIR / 'media'
-STATICFILES_DIRS = [
-    BASE_DIR/ 'static'
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
