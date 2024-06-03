1d287649f22a9cb31be69110546d39909199d2dc
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 08:50:57 +0000

    make migrations

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-# install all the required packages
-pip3 install -r requirements.txt 
+pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
 python3.9 manage.py collectstatic
