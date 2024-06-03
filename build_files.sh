54dd6d385e42ecb0e422512c29bb10b6ee26fde6
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 08:18:30 +0000

    Downgrade django

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@
 pip3 install -r requirements.txt 
 
 # make migrations
-python3.10 manage.py migrate 
-python3.10 manage.py collectstatic
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
