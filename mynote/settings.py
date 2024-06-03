e3fd74653bb0164268a3b7880d2940d55ccde381
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 09:04:14 +0000

    test build

--- mynote/settings.py
+++ mynote/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static', BASE_DIR / 'staticfiles_build'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [
     BASE_DIR/ 'static'
