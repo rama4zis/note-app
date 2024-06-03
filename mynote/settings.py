c1126545bcd791d490f2cf05e11a00f5df24d529
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 15:45:44 +0000

    import cloudinary settings

--- mynote/settings.py
+++ mynote/settings.py
@@ -13,6 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 from pathlib import Path
+import cloudinary_storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
