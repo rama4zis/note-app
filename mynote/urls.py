315a3cf0823a34e5595b42c6286469920493795c
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-06-03 10:10:38 +0000

    Change url

--- mynote/urls.py
+++ mynote/urls.py
@@ -22,8 +22,8 @@ from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
@@ -31,5 +31,5 @@ urlpatterns = [
     path('users/', include('users.urls'))
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
